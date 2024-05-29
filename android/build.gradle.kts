6ba4a1f3bbaf6a22ed89da5ae7d32cf991777725
Norman Breau <norman@breautek.com>
2024-05-28 22:45:45 -0300

    gradle 8.7

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,3 +3,8 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
 }
+
+tasks.wrapper {
+    distributionType = Wrapper.DistributionType.BIN
+    gradleVersion = "8.7"
+}
