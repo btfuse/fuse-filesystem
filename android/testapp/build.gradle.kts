b9ed1f818113d0f79a2b4dac1b56e3b8f138eb8e
Norman Breau <norman@breautek.com>
2024-05-28 22:56:34 -0300

    fix tests and min sdk to match core fuse

--- android/testapp/build.gradle.kts
+++ android/testapp/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.breautek.fuse.filesystem"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
